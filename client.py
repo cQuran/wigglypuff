8ea74b1cec97839aad3a4931d4e3e01f548be055
Muhammad Abdan Mulia <155150200111094@mail.ub.ac.id>
2020-12-18 14:09:28 +0700

    [UPDATE] client sample in python

--- client.py
+++ client.py
@@ -3,18 +3,34 @@ import websockets
 import json
 import sys
 
-
-async def hello(uri):
+async def room_master(uri):
     async with websockets.connect(uri) as websocket:
-
-        # await websocket.send(json.dumps({"action": "ClickAya", "aya": 1}))
-        await websocket.send(json.dumps({"action": "OfferCorrection", "uuid": sys.argv[1]}))
-        print("SENT")
+        await websocket.send(json.dumps({"action": "ClickAya", "aya": 1}))
+        await websocket.send(json.dumps({"action": "MuteAllUser"}))
+        await websocket.send(json.dumps({"action": "MuteUser", "uuid": "abdan"}))
+        await websocket.send(json.dumps({"action": "MoveSura", "id_quran": 1}))
         while True:
-            a = await websocket.recv()
-            print(a)
+            message = await websocket.recv()
+            print(message)
+            message_json = json.loads(message)
+            if message_json['action'] == "OfferCorrection":
+                message_json['action'] = "AnswerCorrection"
+                message_json['result'] = True
+                await websocket.send(json.dumps(message_json))
+                
 
+async def client(uri):
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(json.dumps({"action": "OfferCorrection", "uuid": "abdan"}))
+        while True:
+            message = await websocket.recv()
+            print(message)
 
-asyncio.get_event_loop().run_until_complete(
-    hello("ws://0.0.0.0:6040/api/room/join/dssn/" + sys.argv[1])
-)
+if sys.argv[1] == "room_master":
+    asyncio.get_event_loop().run_until_complete(
+        room_master("ws://0.0.0.0:6040/api/room/join/dssn/" + sys.argv[2])
+    )
+elif sys.argv[1] == "user":
+    asyncio.get_event_loop().run_until_complete(
+        client("ws://0.0.0.0:6040/api/room/join/dssn/" + sys.argv[2])
+    )
