98c7c0dac74ac735c64928467f79bfd5959626f9
Muhammad Abdan Mulia <155150200111094@mail.ub.ac.id>
2020-12-21 19:09:17 +0700

    [UPDATE] change forbidden message

--- src/constants.rs
+++ src/constants.rs
@@ -4,6 +4,6 @@ pub const MESSAGE_ROOM_DOESNT_EXIST: &str = "room doesn't exist / deleted";
 pub const MESSAGE_ROOM_CREATED: &str = "room created";
 pub const MESSAGE_ROOM_DELETED: &str = "room deleted";
 pub const MESSAGE_USER_KICKED: &str = "user kicked";
-pub const MESSAGE_FORBIDDEN_AUTHZ: &str = "you are not allowed to emit this message, closed automatically";
+pub const MESSAGE_FORBIDDEN_AUTHZ: &str = r#"{"action":"Forbidden","message":"you are not allowed to emit this message, closed automatically"}"#;
 pub const FCM_API_TOKEN_KEY: &str = "okk";
 // pub const ROOM_MASTER_ACCES: [&'static str; 3] = &[""];
