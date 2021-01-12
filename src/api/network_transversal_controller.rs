59d5b5e9ccabcbb023f5b341df10763f21a35017
Muhammad Abdan Mulia <155150200111094@mail.ub.ac.id>
2021-01-13 02:16:33 +0700

    [UPDATE] set NAT to dynamically

--- src/api/network_transversal_controller.rs
+++ src/api/network_transversal_controller.rs
@@ -6,24 +6,16 @@ use actix_web::{HttpResponse, Result};
 
 pub async fn get_stun_address() -> Result<HttpResponse> {
     let stun_address = network_transversal::Stun {
-        address: "stun:global.stun.twilio.com:3478?transport=udp".to_string(),
+        address: constants::STUN_SERVER.to_string(),
     };
     let mut turns = Vec::new();
-    turns.push(network_transversal::Turn {
-        address: "turn:global.turn.twilio.com:3478?transport=udp".to_string(),
-        username: "e994ab564e859690d6e325ae7b2e08b0b42ac468836921fc7db5ebb2d080958c".to_string(),
-        credential: "e994ab564e859690d6e325ae7b2e08b0b42ac468836921fc7db5ebb2d080958c".to_string(),
-    });
-    turns.push(network_transversal::Turn {
-        address: "turn:global.turn.twilio.com:3478?transport=tcp".to_string(),
-        username: "e994ab564e859690d6e325ae7b2e08b0b42ac468836921fc7db5ebb2d080958c".to_string(),
-        credential: "e994ab564e859690d6e325ae7b2e08b0b42ac468836921fc7db5ebb2d080958c".to_string(),
-    });
-    turns.push(network_transversal::Turn {
-        address: "turn:global.turn.twilio.com:443?transport=tcp".to_string(),
-        username: "e994ab564e859690d6e325ae7b2e08b0b42ac468836921fc7db5ebb2d080958c".to_string(),
-        credential: "e994ab564e859690d6e325ae7b2e08b0b42ac468836921fc7db5ebb2d080958c".to_string(),
-    });
+    for turn in constants::TURN_ADDRESS.iter() {
+        turns.push(network_transversal::Turn {
+            address: turn.to_string(),
+            username: constants::TURN_USERNAME.to_string(),
+            credential: constants::TURN_CREDENTIAL.to_string(),
+        });
+    }
     let network_transversal = network_transversal::NetworkTransversal {
         stun: stun_address,
         turn: turns,
--- src/constants.rs
+++ src/constants.rs
@@ -7,6 +7,13 @@ pub const MESSAGE_USER_KICKED: &str = "user kicked";
 pub const MESSAGE_FORBIDDEN_AUTHZ: &str = r#"{"action":"Forbidden","message":"you are not allowed to emit this message, closed automatically"}"#;
 pub const FCM_API_TOKEN_KEY: &str = "okk";
 pub const STUN_SERVER: &str = "stun://global.stun.twilio.com:3478?transport=udp";
+pub const TURN_CREDENTIAL: &str = "e994ab564e859690d6e325ae7b2e08b0b42ac468836921fc7db5ebb2d080958c";
+pub const TURN_USERNAME: &str = "e994ab564e859690d6e325ae7b2e08b0b42ac468836921fc7db5ebb2d080958c";
+pub const TURN_ADDRESS: [&str; 3] = [
+    "turn://global.turn.twilio.com:3478?transport=udp",
+    "turn://global.turn.twilio.com:3478?transport=tcp",
+    "turn://global.turn.twilio.com:443?transport=tcp",
+];
 
 pub const PLUGIN_WEBRTC: [&str; 10] = [
     "videotestsrc",
